98d76e07361b50633ed8262a89b9d3db457d2065
QUentinee <que.heude@gmail.com>
2023-10-23 18:57:21 +0200

    TP4

---
+++ src/personnages/Humain.java
@@ -0,0 +1,39 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson_fav;
+	private int argent;
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	public void parler(String texte) {
+		System.out.println(texte);
+	}
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + this.nom + "et j'aime boire du " + this.boisson_fav);
+	}
+	public void boire() {
+		parler("Mmmm, un bon verre de " + this.boisson_fav + " ! GLOUPS !");
+	}
+	public void gagnerArgent(int gain) {
+		this.argent += gain;
+	}
+	public void perdreArgent(int perte) {
+		this.argent += perte;
+	}
+	public void acheter(String bien, int prix) {
+		if(argent >= prix) {
+			parler("J'ai " + this.argent + " sous en poche. Je vais pouvoir m'offrir un " + bien + " à " + prix + "sous");
+			argent -= prix;
+		}
+		else {
+			parler("Je n'ai plus que " + this.argent + "sous en poche. Je ne peux même pas m'offrir un " + bien);
+		}
+	}
+}
+
